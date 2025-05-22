c86668435daed412d1114938be3d19967ba0509a
vidit0018 <vidit.msit@gmail.com>
2025-05-22 15:26:40 +0530

    Cors allowed from all origins

--- server.js
+++ server.js
@@ -17,8 +17,12 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
